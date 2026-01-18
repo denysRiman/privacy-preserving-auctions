df46396d332b550dc63237f7bb9e9d2d030ff332
nahornyi <nahornyi@walter-group.com>
2026-01-18 20:35:54 +0100

    first commit

--- contract/src/Counter.sol
+++ contract/src/Counter.sol
@@ -3,8 +3,14 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function setNumber(uint256 newNumber) public {
+        require(msg.sender == owner, "Not the owner!");
         number = newNumber;
     }
 
--- contract/test/Counter.t.sol
+++ contract/test/Counter.t.sol
@@ -7,18 +7,29 @@ import {Counter} from "../src/Counter.sol";
 contract CounterTest is Test {
     Counter public counter;
 
+    address public garbler =  address(0x1);
+    address public evaluator = address(0x2);
+
     function setUp() public {
+        vm.startPrank(garbler);
         counter = new Counter();
         counter.setNumber(0);
+        vm.stopPrank();
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function test_OwnerCanSetNumber() public {
+        vm.prank(garbler);
+        counter.setNumber(10);
+        assertEq(counter.number(), 10);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
+    function test_NonOwnerCanNotSetNumber(uint256 x) public {
+        uint256 numberBefore = counter.number();
+
+        vm.prank(evaluator);
+        vm.expectRevert("Not the owner!");
         counter.setNumber(x);
-        assertEq(counter.number(), x);
+
+        assertEq(counter.number(), numberBefore);
     }
 }
